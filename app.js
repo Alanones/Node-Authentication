b54796daf37c365e42067a6101af21e78057f9f0
Alan <alanones02@gmail.com>
2022-10-21 16:53:39 +0300

    Level-2 with env variables

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+var encrypt = require('mongoose-encryption');
+
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
-
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email:String,
   password:String
-}
+});
+
+
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model('User', userSchema);
-
-
 app.get('/', (req, res)=>{
   res.render('home');
 });
